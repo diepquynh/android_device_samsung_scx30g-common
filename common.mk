bdc4de999b44e89c7debe0cbb5c74d1aa2b4b2ca
Remilia Scarlet <remilia.1505@gmail.com>
2017-03-12 18:05:13 +0700

    cm-14.1: Initial commit
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

---
+++ common.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2017 The Lineage Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from scx35-common device configuration
+$(call inherit-product, device/samsung/scx35-common/common.mk)
+
+# Inherit scx30g-common vendor tree
+$(call inherit-product-if-exists, vendor/samsung/scx30g-common/scx30g-common-vendor.mk)
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio_hw.xml \
+	audio_para \
+	audio_policy.conf \
+	codec_pga.xml \
+	tiny_hw.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	bt_vendor.conf
+
+# GPS
+PRODUCT_PACKAGES += \
+	gps.xml \
+
+# WiFi
+PRODUCT_PACKAGES += \
+	wpa_supplicant_ss.conf \
+	nvram_net.txt \
+	nvram_mfg.txt
