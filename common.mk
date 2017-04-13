797bcaa8ac94f8fa4eb216799f09d5f65d8f749c
Remilia Scarlet <remilia.1505@gmail.com>
2017-04-13 22:09:05 +0700

    configs: Move some audio configs to scx35-common
    We're ready to use sc8830 audio HAL, which doesn't work with us before
    
    Change-Id: I3b03e9b9989e50d675b6b269446e5520c2c7bc48
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- common.mk
+++ common.mk
@@ -20,10 +20,6 @@ $(call inherit-product-if-exists, vendor/samsung/scx30g-common/scx30g-common-ven
 
 # Audio
 PRODUCT_PACKAGES += \
-	audio_hw.xml \
-	audio_para \
-	audio_policy.conf \
-	codec_pga.xml \
 	tiny_hw.xml
 
 # Bluetooth
