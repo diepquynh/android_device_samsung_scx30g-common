684a930008c8c2abb381b03b836c309fda079a8b
Remilia Scarlet <remilia.1505@gmail.com>
2017-08-18 16:18:16 +0000

    configs: Import media_codecs
    Change-Id: Ib66894f708658770a54d41ee0519e29c011e7e85
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- configs/media/Android.mk
+++ configs/media/Android.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2017 The Lineage Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,31 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit from scx35-common device configuration
-$(call inherit-product, device/samsung/scx35-common/common.mk)
+LOCAL_PATH := $(my-dir)
 
-# Inherit scx30g-common vendor tree
-$(call inherit-product-if-exists, vendor/samsung/scx30g-common/scx30g-common-vendor.mk)
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-	bt_vendor.conf
-
-# GPS
-PRODUCT_PACKAGES += \
-	gps.xml \
-
-# WiFi
-PRODUCT_PACKAGES += \
-	wpa_supplicant.conf \
-	nvram_net.txt \
-	nvram_mfg.txt
-
-# Rootdir
-PRODUCT_PACKAGES += \
-	fstab.sc8830
-
-# Media
-PRODUCT_PACKAGES += \
-	media_codecs.xml
+include $(CLEAR_VARS)
+LOCAL_MODULE := media_codecs.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
