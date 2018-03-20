fd70372e49d448fa2014f42e0fc04e7cda1b030e
HASH <hasherror404@gmail.com>
2018-03-20 21:17:09 +0530

    scx30g-common: Add grandneove3g
    Change-Id: Ic0dea698a0a25ef195a4a1d4b5b01768e45f1552
    Signed-off-by: HASH <hasherror404@gmail.com>

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter core33g,$(TARGET_DEVICE)),)
+ifneq ($(filter core33g grandneove3g,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
